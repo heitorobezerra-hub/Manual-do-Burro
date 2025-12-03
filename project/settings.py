cd6e35384787be2f171c12799a9102021bcaef6c
miguelCapeleiro <heitor.obezerra@gmail.com>
2025-12-03 11:17:01 -0300

    commit de mais uma etapa do projeto

--- project/settings.py
+++ project/settings.py
@@ -125,9 +125,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/accounts/login'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
